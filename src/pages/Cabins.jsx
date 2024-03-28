321565b0ac356ea49e4074106e74a58707b006f3
Andrey Ziuzin <andreyziuzin@Andreys-MacBook-Pro-2.local>
2024-03-28 22:07:56 +0300

    feat: add react query, fetch cabin data

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,21 +1,18 @@
-import { useEffect } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-import { getCabins } from "../services/apiCabins";
+import CabinTable from "../features/cabins/CabinTable";
 
 function Cabins() {
-  useEffect(function () {
-    getCabins().then((data) => console.log(data));
-  }, []);
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-      <img
-        src="https://oproabhrvxbyshrejqth.supabase.co/storage/v1/object/public/Cabin/cabin-001.jpg?t=2024-03-28T13%3A37%3A00.358Z"
-        alt=""
-      />
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+        <p>Filter / Sort</p>
+      </Row>
+      <Row type="horizontal">
+        <CabinTable />
+      </Row>
+    </>
   );
 }
 
