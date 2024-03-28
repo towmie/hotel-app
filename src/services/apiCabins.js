98432abd6c3314903c86862c89dbd44318ed0143
Andrey Ziuzin <andreyziuzin@Andreys-MacBook-Pro-2.local>
2024-03-28 16:39:43 +0300

    feat: integrate supabase

---
+++ src/services/apiCabins.js
@@ -0,0 +1,11 @@
+import supabase from "./supabase";
+
+export async function getCabins() {
+  const { data, error } = await supabase.from("Cabins").select("*");
+
+  if (error) {
+    console.error(error);
+    throw new Error("Cabins couldn't be loaded");
+  }
+  return data;
+}
