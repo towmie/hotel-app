aa5b94ef82264a3a3272427edbe43a15a3322a31
Andrey Ziuzin <andreyziuzin@Andreys-MacBook-Pro-2.local>
2024-04-02 13:47:22 +0300

    refactor: add close fn as custom hook

--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -1,10 +1,13 @@
 import { useContext } from "react";
 import { useState } from "react";
 import { cloneElement } from "react";
+import { useRef } from "react";
+import { useEffect } from "react";
 import { createContext } from "react";
 import { createPortal } from "react-dom";
 import { HiXMark } from "react-icons/hi2";
 import styled from "styled-components";
+import { useOutsideClick } from "../hooks/useOutsideClick";
 
 const StyledModal = styled.div`
   position: fixed;
@@ -76,10 +79,13 @@ function Open({ children, opens: opensWindowName }) {
 
 function Window({ children, name }) {
   const { openName, close } = useContext(ModalContext);
+  const ref = useOutsideClick(close);
+
   if (name !== openName) return null;
+
   return createPortal(
     <Overlay>
-      <StyledModal>
+      <StyledModal ref={ref}>
         <Button onClick={close}>
           <HiXMark />
         </Button>
