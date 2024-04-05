e331e6f50f39eaa92bd4e2da68d0a16fbe56b9e3
Andrey Ziuzin <andreyziuzin@Andreys-MacBook-Pro-2.local>
2024-04-05 17:24:18 +0300

    feat: complete pagination item sort

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,8 +1,11 @@
+import { PAGE_SIZE } from "../utils/constants";
 import { getToday } from "../utils/helpers";
 import supabase from "./supabase";
 
-export async function getBookings({ filter, sortBy }) {
-  let query = supabase.from("bookings").select("*, Cabins(*), Guests(*)");
+export async function getBookings({ filter, sortBy, page }) {
+  let query = supabase
+    .from("bookings")
+    .select("*, Cabins(*), Guests(*)", { count: "exact" });
 
   if (filter) query = query[filter.method || "eq"](filter.field, filter.value);
 
@@ -11,13 +14,19 @@ export async function getBookings({ filter, sortBy }) {
       ascending: sortBy.direction === "asc",
     });
 
-  const { data, error } = await query;
+  if (page) {
+    const from = (page - 1) * PAGE_SIZE;
+    const to = from + PAGE_SIZE - 1;
+    query = query.range(from, to);
+  }
+
+  const { data, error, count } = await query;
 
   if (error) {
     console.error(error);
     throw new Error("Bookings couldn't be loaded");
   }
-  return data;
+  return { data, count };
 }
 
 export async function getBooking(id) {
--- src/ui/Pagination.jsx
+++ src/ui/Pagination.jsx
@@ -1,6 +1,7 @@
 import { HiChevronLeft, HiChevronRight } from "react-icons/hi2";
 import { useSearchParams } from "react-router-dom";
 import styled from "styled-components";
+import { PAGE_SIZE } from "../utils/constants";
 
 const StyledPagination = styled.div`
   width: 100%;
@@ -58,8 +59,6 @@ const PaginationButton = styled.button`
   }
 `;
 
-const PAGE_SIZE = 10;
-
 function Pagination({ count }) {
   const [searchParams, setSearchParams] = useSearchParams();
   const currentPage = !searchParams.get("page")
@@ -72,7 +71,6 @@ function Pagination({ count }) {
     const next = currentPage === pageCount ? currentPage : currentPage + 1;
 
     searchParams.set("page", next);
-    console.log(currentPage, currentPage + 1);
     setSearchParams(searchParams);
   }
 
