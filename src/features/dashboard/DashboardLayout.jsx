8241e961ec5ab8c0765e2f8f03d88656242bc9ea
Andrey Ziuzin <andreyziuzin@Andreys-MacBook-Pro-2.local>
2024-04-23 19:08:19 +0300

    feat: add line chart

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -4,6 +4,7 @@ import Spinner from "../../ui/Spinner";
 import { useRecentStays } from "./useRecentStays";
 import Stats from "./Stats";
 import { useCabins } from "../cabins/useCabins";
+import SalesChart from "./SalesChart";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -33,7 +34,7 @@ function DashboardLayout() {
         cabinCount={cabins.length}
       />
       <div>Activities</div>
-      <div>Duration</div>
+      <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
   );
 }
