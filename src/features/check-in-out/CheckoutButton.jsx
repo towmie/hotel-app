39c51a0a7146d264d49bbef4d8c0fe1d712923ed
Andrey Ziuzin <andreyziuzin@Andreys-MacBook-Pro-2.local>
2024-04-24 13:43:45 +0300

    feat: add Error boundary

--- src/features/check-in-out/CheckoutButton.jsx
+++ src/features/check-in-out/CheckoutButton.jsx
@@ -1,8 +1,15 @@
 import Button from "../../ui/Button";
+import { useCheckOut } from "./useCheckOut";
 
 function CheckoutButton({ bookingId }) {
+  const { checkout, isCheckout } = useCheckOut();
   return (
-    <Button variation="primary" size="small">
+    <Button
+      variation="primary"
+      size="small"
+      onClick={checkout(bookingId)}
+      disabled={isCheckout}
+    >
       Check out
     </Button>
   );
--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -6,6 +6,7 @@ import Stats from "./Stats";
 import { useCabins } from "../cabins/useCabins";
 import SalesChart from "./SalesChart";
 import DurationChart from "./DurationChart";
+import TodayActivity from "../check-in-out/TodayActivity";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -16,18 +17,12 @@ const StyledDashboardLayout = styled.div`
 
 function DashboardLayout() {
   const { bookings, isLoading: isLoading1 } = useRecentBookings();
-  const {
-    stays,
-    confirmedStays,
-    isLoading: isLoading2,
-    numDays,
-  } = useRecentStays();
+  const { confirmedStays, isLoading: isLoading2, numDays } = useRecentStays();
 
   const { cabins, isLoading: isLoading3 } = useCabins();
 
   if (isLoading1 || isLoading2 || isLoading3) return <Spinner />;
 
-  console.log(confirmedStays);
   return (
     <StyledDashboardLayout>
       <Stats
@@ -36,6 +31,7 @@ function DashboardLayout() {
         numDays={numDays}
         cabinCount={cabins.length}
       />
+      <TodayActivity />
       <DurationChart confirmedStays={confirmedStays} />
       <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
